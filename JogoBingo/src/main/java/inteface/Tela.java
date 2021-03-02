2e2016391d914d293a6d462688191622e79d29ac
ketleen <ketleen@ketleen>
2021-03-01 22:07:49 -0300

    iniciei uma tentaiva de interface e estou tentando adicionar imagem na JPanel principal

---
+++ JogoBingo/src/main/java/inteface/Tela.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inteface;
+
+import java.awt.BorderLayout;
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author ketleen
+ */
+public class Tela extends JFrame {
+
+    private final JPanel painelBingo;
+    private final JLabel bingo;
+    public Tela() {
+        this.painelBingo = new JPanel();
+        this.bingo = new JLabel();
+    }
+
+    private void paintPanel() {
+           this.painelBingo.setPreferredSize(new Dimension(300, 60));
+           this.bingo.setIcon(new ImageIcon("/home/ketleen/Documentos/BingoOO certo/JogoB"));
+      
+    }
+
+  
+    private void paintFrame() {
+        paintPanel();
+        this.painelBingo.add(bingo);
+        add(this.painelBingo, BorderLayout.CENTER);
+    }
+
+    public static void main(String[] args) {
+
+        Tela telainicial = new Tela();
+
+        telainicial.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        telainicial.setExtendedState(MAXIMIZED_BOTH);
+        telainicial.setLayout(new BorderLayout());
+        telainicial.paintFrame();
+
+        telainicial.setVisible(true);
+    }
+
+    
+
+}
